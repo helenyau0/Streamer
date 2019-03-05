f4f219f81c17fd54d8c1c1161535d2b150371751
helen <>
2019-03-05 15:10:24 -0800

    create GoogleAuth component and wire up google api lib

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId:
+          '859532953806-8ealmbpmshr2615t6g6v60g8ddt7eadb.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
