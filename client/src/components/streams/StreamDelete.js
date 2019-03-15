931f5dab1d81338262d80a1a25f47dd61c24912d
helen <>
2019-03-15 12:29:04 -0700

    connect deleteStream action creator to delete button

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, deleteStream } from '../../actions';
+import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
 
@@ -10,12 +11,18 @@ class StreamDelete extends React.Component {
   }
 
   renderActions() {
+    const { id } = this.props.match.params;
     return (
       <React.Fragment>
-        <button onSubmit={this.onSubmit} className="ui button negative">
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui button negative"
+        >
           Delete
         </button>
-        <button className="ui button">Cancel</button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
       </React.Fragment>
     );
   }
@@ -48,5 +55,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, deleteStream }
 )(StreamDelete);
